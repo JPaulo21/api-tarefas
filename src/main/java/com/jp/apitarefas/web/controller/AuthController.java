4e6ef7fd57029aef9e36141ce4d2fbfb63ae7afc
JJoÃão Paulo <jp.sport17@gmail.com>
2024-02-05 23:00:59 -0300

    feat: adicionado controller para autenticar usuário e dto dos dados para login

---
+++ src/main/java/com/jp/apitarefas/web/controller/AuthController.java
@@ -0,0 +1,37 @@
+package com.jp.apitarefas.web.controller;
+
+import com.jp.apitarefas.web.dto.LoginDTO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody LoginDTO loginDTO){
+        try{
+            var authToken = new UsernamePasswordAuthenticationToken(loginDTO.username(), loginDTO.password());
+            Authentication auth = authenticationManager.authenticate(authToken);
+            return ResponseEntity.accepted().build();
+        }catch(AuthenticationException e){
+            log.warn("Erro de autenticação: {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
+
+}
