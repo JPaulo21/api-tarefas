0dfc3f0f6c0163807e28de7620921ca7cf3079d6
João Paulo <jp.sport17@gmail.com>
2024-02-12 00:19:56 -0300

    feat: Implementado DTO para consulta de dados da Tarefa e mappendo para entity

--- src/main/java/com/jp/apitarefas/mapper/TarefaMapper.java
+++ src/main/java/com/jp/apitarefas/mapper/TarefaMapper.java
@@ -3,9 +3,12 @@ package com.jp.apitarefas.mapper;
 import com.jp.apitarefas.entity.Tarefa;
 import com.jp.apitarefas.entity.User;
 import com.jp.apitarefas.service.UserService;
-import com.jp.apitarefas.web.dto.TarefaDTO;
+import com.jp.apitarefas.web.dto.tarefa.TarefaConsultaDTO;
+import com.jp.apitarefas.web.dto.tarefa.TarefaDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring", uses = {UserService.class})
 public interface TarefaMapper {
@@ -14,7 +17,10 @@ public interface TarefaMapper {
     @Mapping(target = "enable", constant = "true")
     Tarefa toTarefa(TarefaDTO tarefaDTO);
 
+    List<TarefaConsultaDTO> toListTaretaConsultaDTO(List<Tarefa> tarefas);
+
     default User mapUserIdToUser(Integer userId, UserService userService){
         return userService.findById(userId);
     }
+
 }
---
+++ src/main/java/com/jp/apitarefas/web/dto/tarefa/TarefaConsultaDTO.java
@@ -0,0 +1,14 @@
+package com.jp.apitarefas.web.dto.tarefa;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDateTime;
+
+public record TarefaConsultaDTO(
+        @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+        LocalDateTime dateRegister,
+        String describe
+) {
+}
